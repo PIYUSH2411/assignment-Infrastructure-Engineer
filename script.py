722073068f35e955fa48b8d6f24284744b4c51ac
Piyush Gupta <piyushgupta2411@gmail.com>
2018-02-20 08:43:15 -0800

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
 import glob, os, shutil
-import win32api
+import win32api   #for accessing all drives
+
+#---------code for moving the files from desktop to documents folder----------
 
 os.chdir('C:/Users/'+os.getlogin()+'/Desktop')
 a = ['INI', 'LNK']
 for file in glob.glob("*.*"):
+
+    #---extracting the file extension in string s
     i = len(file)-1
     s = ''
     k = 0
@@ -13,7 +17,9 @@ for file in glob.glob("*.*"):
         i-=1
     s = s[::-1]
     s = s.upper()
-    if(s not in a):
+    #---file extension is stored in string s
+
+    if(s not in a):  #---for avoiding the shortcuts on desktop---
         newpath = 'C:/Users/'+os.getlogin()+'/Documents/'+s
         if not os.path.exists(newpath):
             os.makedirs(newpath)
@@ -22,27 +28,26 @@ for file in glob.glob("*.*"):
         else:
             os.remove('C:/Users/'+os.getlogin()+'/Desktop/'+file)
 
+#---------code for showing 10 largest files in all directories----------
+
 #print("Enter the directory : ")
 #dir = input()
-
-
 for dir in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
     print(dir)
-    dict = {}
-    b = []
+    b = []   #list to store filesize and filename
     for root, dirs, files in os.walk(dir):
         for f in files:
             a = []
             filename = os.path.join(root, f)
             try:
-                filesize = os.path.getsize(filename) >> 20
+                filesize = os.path.getsize(filename) >> 20  #--to get filesize in MB
             except IOError:
                 print('',end='')
                 continue
             a.append(filesize)
             a.append(filename)
             b.append(a)
-    b = sorted(b, reverse = True)
+    b = sorted(b, reverse = True)   #sort in desending order of filesize
     for i in range (0,10):
         if(i < len(b)):
             print(b[i][0], end = ' MB\t\t')
